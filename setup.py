1266dd36488458e879c983ea8291ad8eb0977c7d
A <avilavate@live.in>
2021-03-18 17:36:17 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Data Science and ML study on Titanic disaster dataset.',
+    author='Avil',
+    license='MIT',
+)
